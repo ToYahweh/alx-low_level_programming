3e49eb272b995e1039f49d28c54b31975d840467
ToYahweh <emereninibiato@gmail.com>
2022-06-30 11:09:11 -0700

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.n>
 /**
- *main - Write '"Programming is like building a multilingual puzzle'
- *Return: Always 0 (success)
- */
+* main - Write '"Programming is like building a multilingual puzzle'
+*Return: Always 0 (success)
+*/
 int main(void)
 {
-  Puts ("\"Programming is like building a multilingual puzzle" );
-return (0)
-  }
+puts ("\"Programming is like building a multilingual puzzle" );
+return (0);
+}
