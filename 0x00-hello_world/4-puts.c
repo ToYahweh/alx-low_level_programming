ba3bd34e43d8f9dd154b99f5b8d77de517e66c4d
ToYahweh <emereninibiato@gmail.com>
2022-06-30 11:02:32 -0700

    task 0-4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.n>
+/**
+ *main - Write '"Programming is like building a multilingual puzzle'
+ *Return: Always 0 (success)
+ */
+int main(void)
+{
+  Puts ("\"Programming is like building a multilingual puzzle" );
+return (0)
+  }
