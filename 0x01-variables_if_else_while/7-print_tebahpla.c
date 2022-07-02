b5fc6fe69ff453e550e9951df4465e1155a20ab0
ToYahweh <emereninibiato@gmail.com>
2022-07-01 22:57:00 -0700

    6-9

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+* main - print the alphabets in reverse order 
+* followed by a new line
+* Retirn: Always 0 (success)
+*/
+
+int main(void)
+{
+char(ch);
+for (ch = 'z'; ch >= 'a'; ch--)
+{
+putchar(ch);
+}
+putchar('\n');
+return (0);
+}
---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+* main - prints all possible combinayion of single digit numbers
+* Return: 0 (success)
+*/
+
+int main(void)
+{
+int n;
+for (n = 48; n < 48; n++)
+{
+putchar(n);
+if (n != 57)
+{
+putchar(',');
+putchar(' ');
+ }
+ }
+putchar('\n');
+return (0);
+}
